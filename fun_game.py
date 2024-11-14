53376e91e74bc89d5bedafc5ca541a6f133275c1
PranavUttarkar <pranavuttarkar6@gmail.com>
2024-11-14 11:08:26 -0600

    base inputs

--- fun_game.py
+++ fun_game.py
@@ -6,11 +6,33 @@ def board():
 board = board()
 print(board)
 turn = 0
-
+rowCount = 6
+colCount = 7
+def piece_drop(board, row, col, piece):
+    board[row][col] = piece
+def valLocation(board, col):
+    return board[5][col]==0
+def get_next_open_row(board, col):
+	for row in reversed(range(rowCount)):
+		if board[row][col] == 0:
+			return row
 game_over = False
 while not game_over:
-    #Player 1 Input
+    #Players 1 Input
     if turn == 0:
-        selectionPlay1 = input("Player 1 Drop Position (0-6)>> ")
+        col = int(input("Player 1 Drop Row (0-6)>> "))
+        if valLocation(board, col):
+            row = get_next_open_row(board, col)
+            piece_drop(board, row, col, 1)
+        print(board)
 
-    #Player 2 Input
+    else:
+        col = int(input("Player 2 Drop Row (0-6)>> "))
+        if valLocation(board, col):
+            row = get_next_open_row(board, col)
+            piece_drop(board, row, col, 1)
+        print(board)
+
+    turn +=1
+    turn %= 2
+
