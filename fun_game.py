a243d38c29b82fa73b41604ca9f34615f2465c49
PranavUttarkar <pranavuttarkar6@gmail.com>
2024-11-14 10:51:57 -0600

    Starting file

--- fun_game.py
+++ fun_game.py
@@ -1,1 +1,16 @@
+import numpy as np
+def board():
+    board = np.zeros((6, 7))
+    return board
 
+board = board()
+print(board)
+turn = 0
+
+game_over = False
+while not game_over:
+    #Player 1 Input
+    if turn == 0:
+        selectionPlay1 = input("Player 1 Drop Position (0-6)>> ")
+
+    #Player 2 Input
